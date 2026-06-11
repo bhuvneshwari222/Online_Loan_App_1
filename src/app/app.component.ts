b14f126d1c83c152d3ac953566eafa5c19aab741
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-06-11 22:17:33 +0530

    t-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Online_Loan_App_1';
+  @ViewChild('loanForm') loanForm !: NgForm;
+  onSubmit(){
+    if(this.loanForm.invalid){
+      return this.loanForm.control.markAllAsTouched()
+    }
+    this.loanForm.resetForm();
+  }
 }
